41b9e290c8c47d408ff80b90cea83e074dacd58f
Aicha Nsangou <Anmanjoya@gmail.com>
2025-01-05 22:53:31 +0100

    lien redirectionné avec succès

--- contactstore/contact/views.py
+++ contactstore/contact/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse  
 
-def test(request):  
-    return HttpResponse("Bismillah.")
+def home(request):  
+    return render(request, 'contact/home.html')
+
+def contactregister(request):
+    return render(request,'contact/contact-register.html')
+
+def store(request):
+    return render(request, 'contact/store.html')
