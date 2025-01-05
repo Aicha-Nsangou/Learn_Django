c8bdb743dafc4c083dc52959cbe95812bcf49417
Aicha Nsangou <Anmanjoya@gmail.com>
2025-01-05 22:37:09 +0100

    Fonctionnalité ajouté avec succès, Alhamdulilah -create simple data base contact

--- contactstore/contact/models.py
+++ contactstore/contact/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+
+class Contact(models.Model):
+
+    name = models.CharField(max_length=50)
+    prenom = models.CharField(max_length=50)
+    telephone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+
